16cdf81f9b9cfbdbc84097e2c7332250d8307753
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-14 16:38:20 +0000

    my solution to Highest Scoring Word kata

---
+++ 6kyu/HighestScoringWord/solution.js
@@ -0,0 +1,20 @@
+function high(sentence){
+    let words = sentence.split(' ');
+    const ALPHABET = ' abcdefghijklmnopqrstuvwxyz';
+    let wordChar = words.map(word => word.split(''));
+    let longestWord = "";
+    let highestWordTotal = 0;
+    
+    for(let i = 0; i < wordChar.length; i++){
+      wordTotal = 0;
+      for(let j = 0; j < wordChar[i].length; j++){
+        wordTotal +=ALPHABET.search(wordChar[i][j]);
+      } //end of WORD
+      if(wordTotal > highestWordTotal){
+        longestWord = words[i];
+        highestWordTotal = wordTotal;
+      }
+      console.log(wordTotal);
+    }
+    return longestWord;
+  }
