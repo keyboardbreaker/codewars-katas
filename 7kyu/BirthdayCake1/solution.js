2c7aded9086367c3c5e611f0761e414694e1cee1
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-14 02:10:41 +0000

    refactored BirthdayCake1 solution with ternary operators

--- 7kyu/BirthdayCake1/solution.js
+++ 7kyu/BirthdayCake1/solution.js
@@ -1,23 +1,14 @@
 // x = no of candles
 // y = number of fallen candles
 function cake(x, y){ 
-    let stringSplit =  y.split('');
-    const ALPHABET = ' abcdefghijklmnopqrstuvwxyz'.split('')
+    let stringSplit =  y;
+    const ALPHABET = ' abcdefghijklmnopqrstuvwxyz';
     
     let total = 0;
     for(let i = 0; i < stringSplit.length; i++){
-      if(i % 2 === 0){
-        total += stringSplit.join('').charCodeAt(i);
-      }
-      else{
-        total += ALPHABET.join('').search(stringSplit[i]);
-      }
+      (i % 2 === 0) ?
+        total += stringSplit.charCodeAt(i)
+        : total += ALPHABET.search(stringSplit[i]);
     }
-    
-    if(total / x > 0.7){
-      return 'Fire!';
-    }
-    else{
-      return 'That was close!';
-    }
+    return (total / x) > 0.7 ? 'Fire!' : 'That was close!';
   }
