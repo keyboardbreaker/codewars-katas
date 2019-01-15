b42cb18980c7b44d3f71cf21cf106df920efa0f5
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-15 01:05:04 +0000

    alternative solution for switch conditional kata

--- 8kyu/Conditional statement - switch/solution.js
+++ 8kyu/Conditional statement - switch/solution.js
@@ -1,24 +1,13 @@
-function howManydays(month){
-    var days;
-    switch (month){
-     case 1:
-     case 3:
-     case 5:
-     case 7:
-     case 8:
-     case 10:
-     case 12:
-       days = 31
-     break;
-     case 2: 
-        days = 28
-     break;
-     case 4: 
-     case 6:
-     case 9:
-     case 11:
-        days = 30
-     break;
+function howManydays(month) {
+    switch (month) {
+      case 4:
+      case 6:
+      case 9: 
+      case 11:
+        return 30
+      case 2:
+        return 28
+      default:
+        return 31
     }
-    return days;
   }
