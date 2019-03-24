6d6b2224b3c1a80f60a53cfcf340a4a949df6cd6
Yu-Jin Tan <y.tan@trafixiq.com>
2019-03-24 18:57:39 +0000

    alt solution3 for basic mathematical operations using string literals

--- 8kyu/Basic Mathematical Operations/solution.ts
+++ 8kyu/Basic Mathematical Operations/solution.ts
@@ -1,10 +1,3 @@
-const ops = {
-    "+": (l, r) => l + r, // key: operation, value: function declaration
-    "-": (l, r) => l - r,
-    "*": (l, r) => l * r,
-    "/": (l, r) => l / r,
-} // object
-export const basicOp = (operation: keyof typeof ops, value1: number, value2: number): number => (
-    ops[operation](value1, value2) //invoking inner functions in ops object based on which operator.
-    //object [key](function invocation with value parameters)
-)
+export function basicOp(operation: string, value1: number, value2: number): number {
+    return eval(`${value1}${operation}${value2}`);
+  }
