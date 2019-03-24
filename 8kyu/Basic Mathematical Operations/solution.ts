99f96614efa645a2f472e6ce895c31dce80bc3c2
Yu-Jin Tan <y.tan@trafixiq.com>
2019-03-24 18:45:07 +0000

    alternative solution 1 for basic mathematical operations

--- 8kyu/Basic Mathematical Operations/solution.ts
+++ 8kyu/Basic Mathematical Operations/solution.ts
@@ -1,14 +1,3 @@
 export function basicOp(operation: string, value1: number, value2: number): number {
-    if(operation === '+') {
-      return value1 + value2;
-    } 
-    else if(operation === '-') {
-      return value1 - value2;
-    }
-    else if(operation === '*') {
-      return value1 * value2;
-    }
-    else if(operation === '/' && value2 !== 0) {
-      return value1 / value2;
-    }
+    return eval(value1.toString()+operation+value2.toString());
   }
