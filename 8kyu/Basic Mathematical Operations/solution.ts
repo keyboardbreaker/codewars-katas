cd32522facfc47e594c46dbfc6990f8e103a08b7
Yu-Jin Tan <y.tan@trafixiq.com>
2019-03-24 18:49:05 +0000

    alt solution2 for basic mathematical operations

--- 8kyu/Basic Mathematical Operations/solution.ts
+++ 8kyu/Basic Mathematical Operations/solution.ts
@@ -1,3 +1,10 @@
-export function basicOp(operation: string, value1: number, value2: number): number {
-    return eval(value1.toString()+operation+value2.toString());
-  }
+const ops = {
+    "+": (l, r) => l + r, // key: operation, value: function declaration
+    "-": (l, r) => l - r,
+    "*": (l, r) => l * r,
+    "/": (l, r) => l / r,
+} // object
+export const basicOp = (operation: keyof typeof ops, value1: number, value2: number): number => (
+    ops[operation](value1, value2) //invoking inner functions in ops object based on which operator.
+    //object [key](function invocation with value parameters)
+)
